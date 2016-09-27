14865fc5994f14fbc77f8eae2cc30c4a5f24d774
Louis Larpin <louis.larpin@gmail.com>
2016-09-27 20:31:19 +0200

    use production server url

--- app/scripts/background/channel.js
+++ app/scripts/background/channel.js
@@ -1,7 +1,7 @@
 import { Socket } from 'phoenix-socket';
 
 export function configureChannel() {
-  let socket = new Socket('ws://localhost:4000/socket');
+  let socket = new Socket('ws://54.154.142.114/socket');
   socket.connect();
 
   let channel = socket.channel('track:*');
