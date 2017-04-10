76fc250d01536e2abf1f0f044e5fe9b915fa92a5
Louis Larpin <louis.larpin@gmail.com>
2017-04-10 15:55:49 +0200

    Fix react warnings

--- app/scripts/background/webpack.config.js
+++ app/scripts/background/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-
   entry: [
     './app/scripts/background/index.jsx'
   ],
--- app/scripts/popup/index.jsx
+++ app/scripts/popup/index.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Store } from 'react-chrome-redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './components/App';
+
+// Needed by material-ui for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 const store = new Store({
   portName: 'RTU'
